ded6ad9f3cb41edd9e1613408a40db4f8eb5603f
Natanna <natannacortez@gmail.com>
2022-07-25 12:32:17 -0300

    feat: creat register and login

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { json } from "express";
+import "./setup.js";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import express, { json } from "express";
+import "express-async-errors";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+import router from "./routes/router.js";
 
 const app = express();
-
 app.use(json());
 app.use(cors());
+app.use(router);
+app.use(errorHandlerMiddleware);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Express server listening on port " + PORT);
-});
+export default app;
